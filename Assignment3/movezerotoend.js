5fc74f2107c912fd1dc43b0f4622ef2329683101
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2024-05-23 13:03:38 +0530

    Update movezerotoend.js

--- Assignment3/movezerotoend.js
+++ Assignment3/movezerotoend.js
@@ -1,21 +1,15 @@
 function moveZerosToEnd(arr) {
     let index = 0; 
-
-
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] !== 0) {
             arr[index] = arr[i];
             index++;
         }
     }
-
     for (let i = index; i < arr.length; i++) {
         arr[i] = 0;
     }
-
     return arr;
 }
-
-
 const arr = [0,1,9,8,4,0,0,2,7,0,6,0,9];
 console.log(moveZerosToEnd(arr)); 
